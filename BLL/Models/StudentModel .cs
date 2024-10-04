4ad62d137007a8c06be9a4507d9bef46c983e340
Bertano <Bertano@DESKTOP-U30M9LL>
2024-10-04 11:02:25 +0300

    Add project files.

---
+++ BLL/Models/StudentModel .cs
@@ -0,0 +1,20 @@
+﻿using BLL.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Models
+{
+    public class StudentModel
+    {
+        public Student Entity { get; set; }
+        public int Id => Entity.Id;
+        public string Name => Entity.Name;
+        public string Surname => Entity.Surname;
+        public DateTime? Birthdate => Entity.BirthDate;
+        public decimal? Gpa => Entity.Gpa;
+
+    }
+}
